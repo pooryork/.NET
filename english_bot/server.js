c70d51cfc54d876e7cf635814518e632240f9f40
Artyom Burdavitsyn <artyom.burdavitsyn@gmail.com>
2019-12-13 01:14:42 +0400

    Минимальная адмика с добавлением слов и картинок

--- english_bot/server.js
+++ english_bot/server.js
@@ -4,6 +4,7 @@ let bodyParser = require("body-parser");
 let urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
+const fileUpload = require('express-fileupload');
 let mysql = require('mysql');
 
 var pool = mysql.createPool({
@@ -28,8 +29,11 @@ function orderByRand() {
 let app = express();
 
 app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static('public'));
+app.use(fileUpload({
+    tempFileDir: '/public/img'
+}));
+//app.use(express.static(__dirname + "/public"));
 //app.use(__dirname + './static', express.static('static'));
 
 app.get('/', (req, res) => {
@@ -59,8 +63,7 @@ app.get('/learn', function (req, res) {
             res.render('main', {
                 typeAuth: 'noLogin',
                 type_content: 'learn',
-                data: res1,
-                emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"]
+                data: res1
             });
 
             // for (i in res1) {
@@ -92,31 +95,38 @@ app.get('/about', function (req, res) {
 
 app.get('/train', function (req, res) {
     let sql = 'SELECT * FROM pictures WHERE topic = ?';
-    pool.query(sql, ['animals'], function (req, res) {
 
-        console.log(res);
-        let words = new Array();
-        let refs = new Array();
-        let topics = new Array();
+    try {
 
-        for (i in res) {
-            //console.log(i);
-            words.push(res[i].word);
-            topics.push(res[i].topic);
-            refs.push(res[i].picture_ref);
-            // console.log(res[i].word);
-            // console.log(res[i].topic);
-            // console.log(res[i].picture_ref);
-        }
-        console.log(words);
-        console.log(topics);
-        console.log(refs);
+        pool.query(sql, ['animals'], function (req, res1) {
 
-    });
-    res.render('main', {
-        typeAuth: 'noLogin',
-        type_content: 'train'
-    });
+            console.log(res1);
+            res_send = res1;
+
+            res.render('main', {
+                typeAuth: 'noLogin',
+                type_content: 'train',
+                data: res1
+            });
+
+            // for (i in res1) {
+            //     //console.log(i);
+            //     words.push(res1[i].word);
+            //     topics.push(res1[i].topic);
+            //     refs.push(res1[i].picture_ref);
+            //     // console.log(res[i].word);
+            //     // console.log(res[i].topic);
+            //     // console.log(res[i].picture_ref);
+            // }
+            // console.log(words);
+            // console.log(topics);
+            // console.log(refs);
+
+        });
+    } catch (e) {
+        console.log('Ууупс ...');
+        console.log(e);
+    }
 });
 
 app.get('/admin', function (req, res) {
@@ -124,6 +134,35 @@ app.get('/admin', function (req, res) {
         typeAuth: 'noLogin',
         type_content: 'admin'
     });
+});
+
+app.post('/admin', urlencodedParser, function (req, res) {
+
+    let data = req.body;
+    let picture_ref = '/public/img/' + data.word.toLowerCase() + '.jpg';
+
+    //дописать проверку, что такой предмет уже есть и что поля не пустые
+
+    let sql_insert = "INSERT INTO pictures (topic, word, picture_ref) VALUES (?, ?, ?)";
+    pool.query(sql_insert, [data.topic, data.word, picture_ref], function (req1, res1) {});
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.picture;
+
+    // Use the mv() method to place the file somewhere on your server
+    let ref = '.' + picture_ref;
+    sampleFile.mv(ref, function (err) {
+        if (err)
+            console.log(err);
+    });
+
+    console.log(data);
+
+    res.render('main', {
+        typeAuth: 'noLogin',
+        type_content: 'admin'
+    });
+
 });
 
 app.post('/register', urlencodedParser, function (req, res) {
